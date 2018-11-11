a9a00aad13fc2dbb1ceae167998e96677262942d
Mohamed Shalan <mohamed.sh3lan_93@hotmail.com>
2018-11-11 21:52:41 +0200

    instentiate prenter

--- app/src/main/java/com/shalan/photoweather/home/HomeActivity.java
+++ app/src/main/java/com/shalan/photoweather/home/HomeActivity.java
@@ -3,13 +3,25 @@ package com.shalan.photoweather.home;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.shalan.photoweather.PhotoWeatherApp;
 import com.shalan.photoweather.R;
+import com.shalan.photoweather.base.BaseActivity;
+import com.shalan.photoweather.data.AppDataManager;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends BaseActivity implements HomeViewInteractor{
+
+    private HomePresenter<HomeViewInteractor> presenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        initPresenter();
+    }
+
+    @Override
+    protected void initPresenter() {
+        AppDataManager dataManager = ((PhotoWeatherApp)getApplicationContext()).getDataManager();
+        presenter = new HomePresenter<HomeViewInteractor>(dataManager, this);
     }
 }

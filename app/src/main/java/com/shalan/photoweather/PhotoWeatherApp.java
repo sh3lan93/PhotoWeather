2e2b1b2c98e54efb02d3d5303bd281f9c53b3208
Mohamed Shalan <mohamed.sh3lan_93@hotmail.com>
2018-11-11 21:18:36 +0200

    add app class

---
+++ app/src/main/java/com/shalan/photoweather/PhotoWeatherApp.java
@@ -0,0 +1,11 @@
+package com.shalan.photoweather;
+
+import android.app.Application;
+
+public class PhotoWeatherApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}

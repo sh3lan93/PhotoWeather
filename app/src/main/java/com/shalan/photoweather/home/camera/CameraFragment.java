9069b95b728edd3f8abb6c919849e54ec62d8e65
Mohamed Shalan <mohamed.sh3lan_93@hotmail.com>
2018-11-11 21:40:33 +0200

    add camera fragment

---
+++ app/src/main/java/com/shalan/photoweather/home/camera/CameraFragment.java
@@ -0,0 +1,57 @@
+package com.shalan.photoweather.home.camera;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.shalan.photoweather.R;
+
+
+public class CameraFragment extends Fragment {
+    private OnFragmentInteractionListener mListener;
+
+    public CameraFragment() {
+        // Required empty public constructor
+    }
+
+    public static CameraFragment newInstance() {
+        CameraFragment fragment = new CameraFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_camera, container, false);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    public interface OnFragmentInteractionListener {
+
+    }
+}

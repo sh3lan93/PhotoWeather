b30a4c751740efb2176474c99d3a7b6c932a7ecf
Mohamed Shalan <mohamed.sh3lan_93@hotmail.com>
2018-11-11 21:52:21 +0200

    add base activity to be asbtract and extended from all app activities

---
+++ app/src/main/java/com/shalan/photoweather/base/BaseActivity.java
@@ -0,0 +1,9 @@
+package com.shalan.photoweather.base;
+
+import android.support.v7.app.AppCompatActivity;
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    protected abstract void initPresenter();
+
+}

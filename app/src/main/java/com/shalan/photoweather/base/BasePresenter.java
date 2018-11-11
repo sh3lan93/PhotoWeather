4d50037d40d60115b2c91a48c270aa617842e515
Mohamed Shalan <mohamed.sh3lan_93@hotmail.com>
2018-11-11 21:30:11 +0200

    implement base presenter behavior

--- app/src/main/java/com/shalan/photoweather/base/BasePresenter.java
+++ app/src/main/java/com/shalan/photoweather/base/BasePresenter.java
@@ -1,6 +1,21 @@
 package com.shalan.photoweather.base;
 
+import com.shalan.photoweather.data.AppDataManager;
+
 public class BasePresenter<V extends BaseViewInteractor> implements BasePresenterInteractor<V> {
     private V baseViewInteractor;
+    private AppDataManager dataManager;
 
+    public BasePresenter(V baseViewInteractor, AppDataManager dataManager) {
+        this.baseViewInteractor = baseViewInteractor;
+        this.dataManager = dataManager;
+    }
+
+    public V getBaseViewInteractor() {
+        return baseViewInteractor;
+    }
+
+    public AppDataManager getDataManager() {
+        return dataManager;
+    }
 }
